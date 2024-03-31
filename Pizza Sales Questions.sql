2532caa8b4d10c6ef55c70d47db27604fed6c17c
Qouyah <165476108+Qouyah@users.noreply.github.com>
2024-03-31 23:40:25 +0100

    Pizza Sales Questions

---
+++ Pizza Sales Questions.sql
@@ -0,0 +1,29 @@
+
+-- QUESTIONS
+
+--KPI REQUIREMENT
+-- What is the Total Revenue? i.e, The sum of the total price of all pizza orders.
+
+-- What is the Average Order Value? i.e, The average amount spent per order.
+
+-- What is the Total Pizza sold? i.e, The sum of the quantities of all Pizzas sold.
+
+-- What is the Total Order? i.e, The Total number of orders placed.
+
+-- What is the Average Pizzas per Order? i.e, The average number of Pizzas sold per order.
+
+
+-- CHARTS REQUIREMENT
+--  What is the Daily Trend for Total Orders?
+
+-- What is the Monthly Trend for Total Orders?
+
+-- What is the Percentage of Sales by Pizza Category?
+
+-- What is the Percentage of Sales by Pizza Size?
+
+-- What is the Total Pizzas Sold by Pizza Category?
+
+-- What are the top 5 best sellers by Revenue, Total Quantity and Total Orders?
+
+-- What are the least/bottom 5 sellers by Revenue, Total Quantity and Total Orders?
