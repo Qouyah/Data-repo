39106644191b8cb3e616f6935970617ce26830fa
Qouyah <165476108+Qouyah@users.noreply.github.com>
2024-04-22 15:01:55 +0100

    Update Pizza_Sales_Query.sql

--- Pizza_Sales_Query.sql
+++ Pizza_Sales_Query.sql
@@ -88,6 +88,7 @@ FROM pizza_sales
 GROUP BY pizza_name
 ORDER BY Total_Orders DESC;
 
+
 -- Borrom 5 Pizzas by Total Orders
 SELECT Top 5 pizza_name, COUNT(DISTINCT order_id) AS Total_Orders
 FROM pizza_sales
